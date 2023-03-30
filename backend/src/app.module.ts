fbc9844de6d81fcd46924cabde70900bd90a4aac
DevArthurSantos <arthursantos.s@hotmail.com>
2023-03-30 19:00:08 -0300

    add: instance controler and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { CustomerController } from './modules/customer/customer.controller';
 import { CustomerService } from './modules/customer/customer.service';
 import { PrismaService } from './modules/database/prisma.service';
+import { InstanceController } from './modules/instance/instance.controller';
+import { InstanceService } from './modules/instance/instance.service';
 import { VerificationService } from './modules/verification/verification.service';
 
 @Module({
   imports: [],
-  controllers: [CustomerController],
-  providers: [PrismaService, CustomerService, VerificationService],
+  controllers: [CustomerController, InstanceController],
+  providers: [
+    PrismaService,
+    VerificationService,
+    CustomerService,
+    InstanceService,
+  ],
 })
 export class AppModule {}
