5008045da8b31d5b58e812ffeb1a6ef5b95fb33f
Arthur Santos <76797139+DevArthurSantos@users.noreply.github.com>
2023-03-30 17:02:07 -0300

    Features/customer (#2)
    * alt: Changed database structure and add new db url
    
    * Refactoring: module complete

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ClientController } from './modules/client/client.controller';
-import { ClientService } from './modules/client/client.service';
+import { CustomerController } from './modules/customer/customer.controller';
+import { CustomerService } from './modules/customer/customer.service';
 import { PrismaService } from './modules/database/prisma.service';
-import { EntityController } from './modules/entity/entity.controller';
-import { EntityService } from './modules/entity/entity.service';
-import { InstanciaController } from './modules/instância/Instancia.controller';
-import { InstanciaService } from './modules/instância/instancia.service';
-import { valideteService } from './modules/validete/validete.service';
+import { VerificationService } from './modules/verification/verification.service';
 
 @Module({
   imports: [],
-  controllers: [ClientController, InstanciaController, EntityController],
-  providers: [
-    PrismaService,
-    ClientService,
-    InstanciaService,
-    EntityService,
-    valideteService,
-  ],
+  controllers: [CustomerController],
+  providers: [PrismaService, CustomerService, VerificationService],
 })
 export class AppModule {}
