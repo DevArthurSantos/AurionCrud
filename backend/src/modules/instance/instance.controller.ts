c69e86d9b82c725d314eec63d3753748b740c6f7
Arthur Santos <76797139+DevArthurSantos@users.noreply.github.com>
2023-03-31 04:08:06 -0300

    Develop (#15)
    * feat: add vercel configs
    
    * add: vercel in gitignore
    
    * Features/instance (#14)
    
    * feat: create a dto of intance
    
    * add: new service from scrape the instance
    
    * add: new router to scrape instance

--- backend/src/modules/instance/instance.controller.ts
+++ backend/src/modules/instance/instance.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Delete, Param } from '@nestjs/common';
+import { InstanceDTO } from './dto/instance.dto';
 import { InstanceCreateDTO } from './dto/instanceCreate.dto';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { InstanceService } from './instance.service';
@@ -8,11 +9,15 @@ export class InstanceController {
   constructor(private readonly InstanceService: InstanceService) {}
 
   @Get(':token/new/:instanceName')
-  async createInstance(@Param() instance: InstanceCreateDTO) {
+  async createInstance(@Param() instance: InstanceDTO) {
     return this.InstanceService.createInstance(instance);
   }
+  @Get(':token/scrape/:instanceName')
+  async scrapeInstance(@Param() instance: InstanceDTO) {
+    return this.InstanceService.scrapeInstance(instance);
+  }
   @Delete(':token/erase/:instanceName')
-  async eraseInstance(@Param() instance: InstanceCreateDTO) {
+  async eraseInstance(@Param() instance: InstanceDTO) {
     return this.InstanceService.eraseInstance(instance);
   }
 }
