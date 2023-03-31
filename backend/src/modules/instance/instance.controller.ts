cf71dfa151a4272a904a1a99d2e1618f968301c9
Arthur Santos <76797139+DevArthurSantos@users.noreply.github.com>
2023-03-31 06:59:10 -0300

    Features/swagger (#16)
    * Develop (#15)
    
    * feat: add vercel configs
    
    * add: vercel in gitignore
    
    * Features/instance (#14)
    
    * feat: create a dto of intance
    
    * add: new service from scrape the instance
    
    * add: new router to scrape instance
    
    * del: old vercel config file
    
    * chore: @nestjs/swagger
    
    * add: nestjs/swagger plugin configs
    
    * add: Docs using swagger
    
    * add: Docs using swagger
    
    * add: Docs using swagger
    
    * add: Docs using swagger

--- backend/src/modules/instance/instance.controller.ts
+++ backend/src/modules/instance/instance.controller.ts
@@ -1,21 +1,79 @@
 import { Controller, Get, Delete, Param } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { InstanceDTO } from './dto/instance.dto';
 import { InstanceCreateDTO } from './dto/instanceCreate.dto';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { InstanceService } from './instance.service';
 
+@ApiTags('instance')
 @Controller('api/v1/instance')
 export class InstanceController {
   constructor(private readonly InstanceService: InstanceService) {}
 
+  @ApiOperation({ summary: 'Utilizado para criar uma nova instancia' })
+  @ApiParam({
+    name: 'instanceName',
+    example: 'Pessoas',
+  })
+  @ApiParam({
+    name: 'Token',
+    example: '44ec4abd-be80-4028-865b-c41e2e475015',
+  })
+  @ApiResponse({
+    status: 200,
+    schema: {
+      type: 'object',
+      properties: {
+        id: { type: 'string' },
+        instance_id: { type: 'string' },
+        customer_token: { type: 'string' },
+      },
+    },
+  })
   @Get(':token/new/:instanceName')
-  async createInstance(@Param() instance: InstanceDTO) {
+  async createInstance(@Param() instance: InstanceCreateDTO) {
     return this.InstanceService.createInstance(instance);
   }
+
+  @ApiOperation({
+    summary: 'Utilizado para listar todos os fragmetos de uma instancia',
+  })
+  @ApiParam({
+    name: 'instanceName',
+    example: 'Pessoas',
+  })
+  @ApiParam({
+    name: 'Token',
+    example: '44ec4abd-be80-4028-865b-c41e2e475015',
+  })
+  @ApiResponse({
+    status: 200,
+    schema: {
+      type: 'object',
+      properties: {
+        intanceID: { type: 'string' },
+        createdAt: { type: 'string' },
+        data: { type: 'object' },
+      },
+    },
+  })
   @Get(':token/scrape/:instanceName')
   async scrapeInstance(@Param() instance: InstanceDTO) {
     return this.InstanceService.scrapeInstance(instance);
   }
+
+  @ApiOperation({ summary: 'Utilizado para deletar uma instancia' })
+  @ApiParam({
+    name: 'instanceName',
+    example: 'Pessoas',
+  })
+  @ApiParam({
+    name: 'Token',
+    example: '44ec4abd-be80-4028-865b-c41e2e475015',
+  })
+  @ApiResponse({
+    status: 200,
+  })
   @Delete(':token/erase/:instanceName')
   async eraseInstance(@Param() instance: InstanceDTO) {
     return this.InstanceService.eraseInstance(instance);
