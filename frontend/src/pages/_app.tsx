c4f295238bb5bf394910b04c35a279baf5e79165
DevArthurSantos <arthursantos.s@hotmail.com>
2023-04-01 03:32:59 -0300

    edit: news configs

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,33 +1,27 @@
 import type { AppProps } from 'next/app';
-
+import { useRouter } from 'next/router'
 
 import Header from '@foundation/patterns/header';
 import Fotter from '@foundation/patterns/footer';
-import light from '@infra/style/themes/light';
 import dark from '@infra/style/themes/dark';
-import usePersistedState from '@infra/utils/Hooks/userPersistedState';
-import { DefaultTheme, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@infra/style/global';
-import { Poppins } from 'next/font/google';
 import Container from './styled';
 
-const poppins = Poppins({ subsets: ['latin'], weight: '600' });
 
 export default function App({ Component, pageProps }: AppProps) {
-	const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', dark);
-
-	function themeHandlerState() {
-		setTheme(theme === dark ? light : dark);
-	}
-
+	const router = useRouter()
+	const hiddenHeader = router.pathname
 	return (
-		<Container className={poppins.className} >
-			<ThemeProvider theme={theme}>
-				<GlobalStyle theme={theme} />
-				<Header themeHandlerState={themeHandlerState} />
-				<Component {...pageProps} />
-				<Fotter />
-			</ThemeProvider>
-		</Container>
+		<>
+			<Container >
+				<ThemeProvider theme={dark}>
+					<GlobalStyle theme={dark} />
+					{hiddenHeader === "/docs" ? null : <Header /> }
+					<Component {...pageProps} />
+					{hiddenHeader === "/docs" ? null : <Fotter /> }
+				</ThemeProvider>
+			</Container>
+		</>
 	);
 }
