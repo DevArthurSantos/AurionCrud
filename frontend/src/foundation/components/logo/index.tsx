8770e4632219d1e6c8c34052bfff8d4cd7021a28
DevArthurSantos <arthursantos.s@hotmail.com>
2023-04-02 19:08:13 -0300

    fix: porject

--- frontend/src/foundation/components/logo/index.tsx
+++ frontend/src/foundation/components/logo/index.tsx
@@ -11,13 +11,14 @@ export type LogoProps = {
 
 function Logo({ sizing = LogoSizing.medium }: LogoProps) {
 	return (
+
 		<>
 			{sizing === LogoSizing.small ?
 				<>
 					<div className="text-sm md:text-sm text-white text-4xl font-bold">
-							AURION<span className="text-orange-500">CRUD</span>
-						</div>
-
+						AURION<span className="text-orange-500">CRUD</span>
+					</div>
+				</>
 				: sizing === LogoSizing.medium ?
 					<>
 						<div className="text-3xl md:text-3xl text-white text-4xl font-bold">
