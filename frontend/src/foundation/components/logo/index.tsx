be9832c749be91d255ff15717e42d8f04fdca186
DevArthurSantos <arthursantos.s@hotmail.com>
2023-04-02 21:32:05 -0300

    edit: text color of logo

--- frontend/src/foundation/components/logo/index.tsx
+++ frontend/src/foundation/components/logo/index.tsx
@@ -15,19 +15,19 @@ function Logo({ sizing = LogoSizing.medium }: LogoProps) {
 		<>
 			{sizing === LogoSizing.small ?
 				<>
-					<div className="text-sm md:text-sm text-white text-4xl font-bold">
+					<div className="text-sm md:text-sm text-black-600 text-4xl font-bold">
 						AURION<span className="text-orange-500">CRUD</span>
 					</div>
 				</>
 				: sizing === LogoSizing.medium ?
 					<>
-						<div className="text-3xl md:text-3xl text-white text-4xl font-bold">
+						<div className="text-3xl md:text-3xl text-black-600 text-4xl font-bold">
 							AURION<span className="text-orange-500">CRUD</span>
 						</div>
 					</>
 					:
 					<>
-						<div className="text-4xl md:text-4xl text-white text-4xl font-bold">
+						<div className="text-4xl md:text-4xl text-black-600 text-4xl font-bold">
 							AURION<span className="text-orange-500">CRUD</span>
 						</div>
 					</>
