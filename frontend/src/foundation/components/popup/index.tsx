e495441891d6a32a294abe6677677fc99a1b5a27
DevArthurSantos <arthursantos.s@hotmail.com>
2023-04-04 09:09:56 -0300

    add: beta info in the popup

--- frontend/src/foundation/components/popup/index.tsx
+++ frontend/src/foundation/components/popup/index.tsx
@@ -7,8 +7,8 @@ function Popup() {
       <div className="animate-fade-in-out opacity-0 text-black-600 bg-white-300 h-screen w-full flex items-center justify-center">
         
         <div className="flex flex-col items-center pt-6 pr-6 pb-6 pl-6">
-          <p className="text-2xl font-semibold leading-none text-white tracking-tighter lg:text-3xl">
-            <Logo sizing={LogoSizing.big} />
+          <p className="text-2xl flex font-semibold leading-none text-white tracking-tighter lg:text-3xl">
+            <Logo sizing={LogoSizing.big} /> beta 
           </p>
           <p className="mt-2 text-xl leading-relaxed text-center text-gray-200">
             Developing your ideas with technology.
